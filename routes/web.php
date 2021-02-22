7a1373a9274cc33938555bebb3bfc7011b22a85d
rpevillamonte <58614550+rpevillamonte@users.noreply.github.com>
2021-02-22 15:04:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,10 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\MaterialsController;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    if (Auth::check())
-        return View::make('welcome');
-    return view('auth.login');
+    return view('welcome');
 });
 
 Auth::routes();
@@ -31,24 +24,27 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //Route::get('/products/{id}', [App\Http\Controllers\ProductsController::class, 'test']);
 
-Route::get('/dashboard', function () { return view('modules.dashboard'); })->name('dashboard');
-Route::get('/manufacturing', function () { return view('modules.manufacturing'); });
-Route::get('/item', function () { return view('modules.item'); });
-Route::get('/inventory', function () { return view('modules.inventory'); });
+Route::get('/dashboard', function () {
+    return view('modules.dashboard');
+});
 
-//*PRODUCT POST METHOD*/
-Route::post('/create-product', 'ProductsController@store');
-Route::post('/create-material', 'MaterialsController@store');
-Route::patch('/update-product/{id}', 'ProductsController@update');
-Route::post('/delete-product/{id}', 'ProductsController@delete');
-Route::post('/delete-material/{id}', 'MaterialsController@delete');
+Route::get('/manufacturing', function () {
+    return view('modules.manufacturing');
+});
 
+Route::get('/item', function () {
+    return view('modules.item');
+});
 
-// added get routes for customer module
-Route::get('/customer', function () { return view('modules.customer'); });
-Route::get('/customertable', function () { return view('modules.customertable'); });
+Route::get('/newbom', function () {
+    return view('modules.newbom');
+});
 
+// Route::get('/inventory', function () {
+//     return view('modules.inventory');
+// });
 
+<<<<<<< Updated upstream
 Route::get('/newbom', function () {
     return view('modules.newbom');
 });
@@ -61,49 +57,27 @@ Route::get('/newbom', function () {
 // added post & update routes for customer module
 Route::post('/create-customer', 'CustomerController@store')->name('customer');
 Route::put('/update-customer/{id}', 'CustomerController@update');
+=======
+>>>>>>> Stashed changes
 
 /*PRODUCTS TABLE ROUTE*/
-Route::get('/item','ProductsController@index');
-Route::get('/get-attribute/{id}', 'ProductsController@get_attribute');
 Route::patch('/create-product', 'ProductsController@store');
 Route::patch('/update-product/{id}', 'ProductsController@update');
-Route::get('/inventory', 'MaterialsController@index')->name('inventory');
-Route::get('/inventory/{id}', 'MaterialsController@get')->name('inventory.specific');
-
-
-/*PRODUCT POST METHOD*/
-Route::post('/create-product', 'ProductsController@store');
-/*MATERIAL POST METHOD*/
-Route::post('/create-material', 'MaterialsController@store');
-/*MATERIAL CATEGORY POST METHOD*/
-Route::post('/create-categories' , 'MaterialsController@storeCategory');
-
-/*DEBUGGING*/
-// Route::post('/create-product', function(Request $request){
-//     echo json_encode($request->all());
-// });
-
-Route::patch('/update-material/{id}', 'MaterialsController@update')->name('material.update');
-Route::patch('/update-product/{id}', 'ProductsController@update');
-
-/*DEBUGGING*/
-// Route::patch('/update-product/{id}', function(Request $request){
-//     echo json_encode($request->all());
-// });
-
 Route::post('/delete-product/{id}', 'ProductsController@delete');
 Route::post('/create-item-group', 'ProductsController@add_item_group');
 Route::post('/create-product-unit', 'ProductsController@add_product_unit');
 Route::post('/create-attribute', 'ProductsController@add_attribute');
+Route::get('/get-attribute/{id}', 'ProductsController@get_attribute');
 
 /*RAW MATERIALS TABLE ROUTE*/
 Route::get('/inventory', 'MaterialsController@index')->name('inventory');
-Route::get('/inventory/{id}', 'MaterialsController@get')->name('inventory.specific');
 Route::post('/create-material', 'MaterialsController@store');
+Route::get('/inventory/{id}', 'MaterialsController@get')->name('inventory.specific');
 Route::post('/delete-material/{id}', 'MaterialsController@delete');
 Route::patch('/update-material/{id}', 'MaterialsController@update')->name('material.update');
 Route::patch('/update-product/{id}', 'ProductsController@update');
 
+<<<<<<< Updated upstream
 
 Route::get('/test', 'DebugController@index');
 Route::post('/test', 'DebugController@debug');
@@ -125,8 +99,13 @@ Route::get('/sign-in/google', 'Auth\LoginController@google');
 Route::get('/sign-in/google/redirect', 'Auth\LoginController@googleRedirect');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
+=======
+>>>>>>> Stashed changes
 /*BOM TABLE ROUTE*/
 Route::post('/createBOM', 'BOMController@store');
 Route::get('/search-product/{product_code}', 'BOMController@search_product');
 Route::get('/bom', 'BOMController@index');
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
